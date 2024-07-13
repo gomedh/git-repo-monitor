57923835f9fdbad4894e281b51d88ff601c0c3ef
Gomedh Tak <gomedh.tak1@gmail.com>
2024-07-13 23:53:16 +0100

    Fix - To supress the 2 api calls when in local dev

--- src/index.js
+++ src/index.js
@@ -6,9 +6,9 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode> : Locally there will be 2 api calls, so to suppress that we do it
     <App />
-  </React.StrictMode>
+  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
